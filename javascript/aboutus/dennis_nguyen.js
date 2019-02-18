17b101cfb17681b5c6c3bb5878f545e6c75c79b9
Dennis N <nguyendp@ucalgary.ca>
2019-02-17 22:47:31 -0700

    added bio details and upped phase 1 PDF

--- javascript/aboutus/dennis_nguyen.js
+++ javascript/aboutus/dennis_nguyen.js
@@ -1,7 +1,7 @@
-document.write("i am dennis");
+document.write("Dennis Nguyem 10073905. I'm not too fond of leaving pictures of myself on the internet so excuse the generic picture. I'm a Computer Science major and I'm taking this course because of an interest in future possibilities for how humans will be able to interact with technology!");
 function dennis_email(){
-	document.write("email");
+	document.write("nguyendp@ucalgary.ca");
 }
 function dennis_phone(){
-	document.write("phone");
+	document.write("587-700-7248");
 }
